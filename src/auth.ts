30a736d67bbd7fb422c1c74f7e0ca4f84e5bd919
balckowl <shiggyjr1492@gmail.com>
2025-07-20 14:44:17 +0900

    fix: 修正したよ

--- src/auth.ts
+++ src/auth.ts
@@ -1,18 +1,7 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { eq } from "drizzle-orm";
 import { db } from "./db";
-import { user } from "./db/schema";
 import { env } from "./env";
-import type { UserSchemaType } from "./server/schemas/user.schema";
-import type { RawPost } from "./types";
-
-const TWITTER_BEARER_TOKEN_List = [
-	env.TWITTER_BEARER_TOKEN_1,
-	env.TWITTER_BEARER_TOKEN_2,
-	env.TWITTER_BEARER_TOKEN_3,
-	env.TWITTER_BEARER_TOKEN_4,
-];
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
@@ -35,15 +24,6 @@ export const auth = betterAuth({
 		github: {
 			clientId: env.GITHUB_CLIENT_ID,
 			clientSecret: env.GITHUB_CLIENT_SECRET,
-			// mapProfileToUser: (profile) => {
-			// 	// 'profile' オブジェクトの具体的な構造はTwitter (X) のAPIドキュメントで確認してください。
-			// 	// ここでは、仮にTwitter (X) のプロフィールから 'name' や 'screen_name' を取得する例です。
-			// 	const data = profile.data;
-			// 	return {
-			// 		username: data.username,
-			// 		twitterId: data.id,
-			// 	};
-			// },
 		},
 	},
 });
